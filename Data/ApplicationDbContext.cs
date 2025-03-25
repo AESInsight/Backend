59cc06530323fd2902028781b0f9a142943c71fe
ChristopherDolan <202305666@post.au.dk>
2025-03-25 10:37:33 +0100

    adds Testdata

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Backend.Models;
 
 namespace Backend.Data;
 
@@ -7,6 +8,16 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<YourModel> YourModels { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Apply seed data
+        DbSeeder.SeedData(modelBuilder);
     }
 
     // Add your DbSet properties here for your entities
