ac7c02a618e2715b1598da2dbafc88c244eca6da
Phaurholt <202305774@post.au.dk>
2025-04-01 12:31:43 +0200

    Removed dummy colon from user.cs

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,6 @@ public class User
     public int UserId { get; set; }
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
-    public string Role { get; set; } = "User"; // Standardrolle
-    public string DummyColumn { get; set; } = "Temp"; // Dummy-kolonne til testformål
+    public string Role { get; set; } = "User"; 
 }
 }
