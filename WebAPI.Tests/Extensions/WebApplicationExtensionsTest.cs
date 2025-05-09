37614063f60e179ccfc9957c99ccd43d25a4dadd
Christopher Dolan <cchristopherdolan@gmail.com>
2025-05-09 12:32:44 +0200

    Tests added not finished

---
+++ WebAPI.Tests/Extensions/WebApplicationExtensionsTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using System.Threading.Tasks;
+using System.Net;
+
+public class MiddlewareTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public MiddlewareTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Swagger_Endpoint_Should_Work_In_Dev()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/swagger/index.html");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
