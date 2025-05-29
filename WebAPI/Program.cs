e11c6ee7adc983d8de27bf1251d8c47af728cc8b
Michael Dao <202307644@post.au.dk>
2025-05-29 19:48:53 +0200

    Test coverage

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -96,11 +96,7 @@ app.MapControllers(); // Map controller routes
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-    if (env != "Testing")
-    {
-        dbContext.Database.Migrate(); // Apply pending migrations to the database
-    }
+    dbContext.Database.Migrate(); // Apply pending migrations to the database
     // Optionally, call a method to seed additional data if needed
 
     // Query company with ID 4
